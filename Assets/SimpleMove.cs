c59c6d7b451b45642c4981bc525ce4fe77615338
Brett Kercher <brettk@unity3d.com>
2019-05-09 15:10:35 -0500

    Basic Game Functionality

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public CharacterController Controller;
+    public float Speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        Controller.SimpleMove(new Vector3(h * Speed, 0, v * Speed));
+    }
+}
