ad9e65e080c770b27b9f57fed05c9a57ee50b065
Brett Kercher <brettk@unity3d.com>
2019-05-09 14:54:43 -0500

    delete some stuff, update git ignore

---
+++ Assets/Editor/BuildScriptHookTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AddressableAssets.Build;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildscriptHookTest
+{
+    [MenuItem("Blah/Blah")]
+    static void NewMenuOption()
+    {
+        BuildScript.buildCompleted += OnBuildComplete;
+    }
+
+    static void OnBuildComplete(object result)
+    {
+        Debug.Log("BuildScript Complete");
+        
+//        var filePaths = result.FileRegistry.GetFilePaths();
+//        foreach (var path in filePaths)
+//        {
+//            Debug.Log(path);
+//        }
+    }
+}
