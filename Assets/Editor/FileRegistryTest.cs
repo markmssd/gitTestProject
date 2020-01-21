e76aff3b081cce54e3ec5fb2e3ec8a7b6998c875
Brett Kercher <brettk@unity3d.com>
2020-01-21 13:15:17 -0600

    latest version of addressables

---
+++ Assets/Editor/FileRegistryTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+
+public class FileRegistryTest : MonoBehaviour
+{
+    [MenuItem("Assets/Test File Registry")]
+    public static void ToggleSimulationMode()
+    {
+        Debug.Log("Hey");
+
+        BuildScript.buildCompleted += result =>
+        {
+            foreach (var path in result.FileRegistry.GetFilePaths())
+            {
+                Debug.Log(path);
+            }
+        };
+        
+        AddressableAssetSettings.BuildPlayerContent();
+    }
+}
